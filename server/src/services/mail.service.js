afe5fb8ca66892d39d623eaf9d9c6133ee9ab249
shash-hq <shashankranjan.yt@gmail.com>
2026-03-27 23:48:57 +0530

    fix: use nodemailer gmail service to bypass ipv6 error

--- server/src/services/mail.service.js
+++ server/src/services/mail.service.js
@@ -2,9 +2,7 @@ import nodemailer from 'nodemailer';
 
 // Transporter setup using your Gmail SMTP credentials
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: process.env.SMTP_PORT || 587,
-  secure: false, // true for 465, false for 587
+  service: 'gmail', 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
